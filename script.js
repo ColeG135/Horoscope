dd0a9f0abebee6d2cb1740b5ebbe3263ac28ec13
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-01-10 14:22:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-var signs = ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"];
-var messages = ["You will have lots of fun","..."];
+var signs = ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer",
+    "Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"];
+
+var messages = ["You will have lots of fun","There is a challenge coming your way","Good things are in your future",
+    "Someone in your family is thinking of you", "Great fortune is coming your way soon", "Don't underestimate yourself",
+    "You will either need or give help to and from a friend","after minsfortune there is fortune","Don't let the little things get to you",
+    "Remember what goes up must come back down","To go forward you must sometimes go back","Don't get ahead of yourself"];
+
 var images = ["images/aquarius.jpg","images/Pisces.jpg", "images/Pisces.jpg",
     "image/Aries.jpg","images/Aries.jpg","images/Taurus.jpg", "images/Gemini.jpg",
     "images/Cancer.jpg","images/leo.jpg","images/Virgo.jpg","images/Libra.jpg",
@@ -28,4 +34,3 @@ function determineSign (month,day) {
         return 0;
     }
 }
-
