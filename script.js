ab6b544a1bcc3bc75622a1b110e05f124231a920
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-01-24 10:25:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ var messages = ["You will have lots of fun","There is a challenge coming your wa
     "You will either need or give help to and from a friend","after minsfortune there is fortune","Don't let the little things get to you",
     "Remember what goes up must come back down","To go forward you must sometimes go back","Don't get ahead of yourself"];
 
-var images = ["images/aquarius.jpg","images/Pisces.jpg", "images/Pisces.jpg",
-    "image/Aries.jpg","images/Aries.jpg","images/Taurus.jpg", "images/Gemini.jpg",
-    "images/Cancer.jpg","images/leo.jpg","images/Virgo.jpg","images/Libra.jpg",
+var image = ["images/aquarius.jpg","images/Pisces.jpg", "image/Aries.jpg","images/Taurus.jpg", "images/Gemini.jpg",
+    "images/Cancer.jpg","images/Leo.jpg","images/Virgo.jpg","images/Libra.jpg",
     "images/Scorpio.jpg","images/Sagittarius.jpg","images/Capricorn.jpg"];
+
 
 function start() {
 
@@ -34,48 +34,88 @@ function determineSign (month,day) {
         return 0;
     }
     if(month == 2 && day < 19 || 20) {
-        return 2;
+        return 1;
     }
     if(month == 3 && day < 21 || 19) {
+        return 2;
+    }
+    if(month == 4 && day < 21 || 1) {
         return 3;
     }
-    if(month == 4 && day < 21 || 20) {
+    if(month == 5 && day < 21) {
         return 4;
     }
-    if(month == 5 && day < 21 || 22) {
+    if(month == 6 && day < 21) {
         return 5;
     }
-    if(month == 6 && day < 23 || 22) {
+    if(month == 7 && day < 21) {
         return 6;
     }
-    if(month == 7 && day < 23 || 22) {
+    if(month == 8 && day < 21) {
         return 7;
     }
-    if(month == 8 && day < 23 || 22) {
+    if(month == 9 && day < 21) {
         return 8;
     }
-    if(month == 9 && day < 23 || 21) {
+    if(month == 10 && day < 21) {
         return 9;
     }
-    if(month == 10 && day < 22 || 21) {
+    if(month == 11 && day < 21) {
         return 10;
     }
-    if(month == 11 && day < 22 || 19) {
+    if(month == 12 && day < 21) {
         return 11;
     }
 }
 
+function image() {
+    if(month == 1) {
+        return "image/aquarius.jpg";
+    }
+    if(month == 2) {
+        return "image/Pisces.jpg";
+    }
+    if(month == 3) {
+        return "image/Aries.jpg";
+    }
+    if(month == 4) {
+        return "image/Taurus.jpg";
+    }
+    if(month == 5) {
+        return "image/Gemini.jpg";
+    }
+    if(month == 6) {
+        return "image/Cancer.jpg";
+    }
+    if(month == 7) {
+        return "image/Leo.jpg";
+    }
+    if(month == 8) {
+        return "image/Virgo.jpg";
+    }
+    if(month == 9) {
+        return "image/Libra.jpg";
+    }
+    if(month == 10) {
+        return "image/Scorpio.jpg";
+    }
+    if(month == 11) {
+        return "image/Sagittarius.jpg";
+    }
+    if(month == 12) {
+        return "image/Capricorn.jpg";
+    }
+}
 
-
-//Aquarius January 20 - February 18 -- 0
-//Pisces February 19 - March 20 -- 1
-//Aries March 21 - April 19 -- 2
-//Taurus April 20 - May 20 -- 3
-//Gemini May 21 - June 20 -- 4
-//Cancer June 21 - July 22 -- 5
-//Leo July 23 - August 22 -- 6
-//Virgo August 23 - September 22 -- 7
-//Libra September 23 - October 22 -- 8
-//Scorpio October 23 - November 21 -- 9
-//Sagittarius November 22 - December 21 -- 10
-//Carpricorn December 22 - January 19 -- 11
+//Aquarius January 20 - February 18
+//Pisces February 19 - March 20
+//Aries March 21 - April 19
+//Taurus April 20 - May 20
+//Gemini May 21 - June 20
+//Cancer June 21 - July 22
+//Leo July 23 - August 22
+//Virgo August 23 - September 22
+//Libra September 23 - October 22
+//Scorpio October 23 - November 21
+//Sagittarius November 22 - December 21
+//Carpricorn December 22 - January 19
